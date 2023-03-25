1c75c71b3cbdd371ecad3c3b7d760e7079939f8a
baizhi958216 <1475289190@qq.com>
2023-03-25 11:33:38 +0800

    p05
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import { p01 } from "./courses/p01";
 import { p02 } from "./courses/p02";
 import { p03 } from "./courses/p03";
 import { p04 } from "./courses/p04";
+import { p05 } from "./courses/p05";
 
 p01();
 p02();
 p03();
 p04();
+p05();
---
+++ src/courses/p05.ts
@@ -0,0 +1,32 @@
+const p05 = () => {
+  /*
+     // 布尔值数组
+    let arr: boolean[] = [true, false];
+    //   或者使用泛型
+    let arr1: Array<boolean> = [true, false];
+    //   定义对象数组
+    interface X {
+        name: string;
+    }
+    let arr3: X[] = [{ name: "小满" }, { name: "胡萝卜" }];
+    //   定义二维数组
+    let arr4: number[][] = [[1], [2], [3]];
+    //   定义多维数组
+    let arr5: number[][][] = [[[1]], [[2]], [[3]]];
+    let arr5_1: Array<Array<Array<number>>> = [[[1]], [[2]], [[3]]];
+    //   大杂烩数组
+    let arr6: any[] = [1, true, "123", {}];
+    // 使用元组
+    let arr7: [number, boolean, string, object] = [1, true, "123", {}];
+    // 函数
+    function a(...args: number[]) {
+        // 剩余参数,解构出来就是数组
+        console.log(args);
+        // 类数组对象, 没有数组的内置方法
+        let a: IArguments = arguments;
+    }
+    a(1, 2, 3, 4, 5); 
+    
+    */
+};
+export { p05 };
